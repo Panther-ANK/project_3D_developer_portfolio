ad417f0a8a2929fd1ab8291abf7bc1c88831d357
Panther-ANK <153408749+Panther-ANK@users.noreply.github.com>
2026-09-15 00:28:25 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Ankur Barik &nbsp;
-            <span className='sm:block hidden'> | Fullstack Developer</span>
+            <span className='sm:block hidden'> | Fullstack Developer | Film Maker</span>
           </p>
         </Link>
 
